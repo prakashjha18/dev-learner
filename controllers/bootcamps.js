5834b61863533e93b267789ca33c0f88245a6afb
Prakash <prkjha25@gmail.com>
2020-09-20 20:25:13 +0530

    advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,9 +4,16 @@ const geocoder = require('../utils/geocoder');
 const ErrorResponse = require('../utils/errorResponse');
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-        const bootcamps = await Bootcamp.find();
-        res.status(200).json({success:true,count:bootcamps.length,data:bootcamps});
-        //next(err);
+
+    let query;
+    let queryStr = JSON.stringify(req.query);
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g,match => `$${match}`);
+
+    query = Bootcamp.find(JSON.parse(queryStr));
+    const bootcamps = await query;
+    res.status(200).json({success:true,count:bootcamps.length,data:bootcamps});
+    //next(err);
 
 });
 

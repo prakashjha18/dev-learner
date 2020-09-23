774154048d19912c427071f9dd90797b5542fc6e
Prakash <prkjha25@gmail.com>
2020-09-23 20:12:18 +0530

    update and delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -65,3 +65,40 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
   });
 });
 
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course
+  });
+});
+
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+  await course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});

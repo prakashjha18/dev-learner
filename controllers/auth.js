297117a81a51a9d2420f3fd4530918a3d438cfc1
Prakash <prkjha25@gmail.com>
2020-10-04 21:23:36 +0530

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,3 +18,31 @@ exports.register = asyncHandler(async(req,res,next) => {
     res.status(200).json({success:true,token:token});
     //sendTokenResponse(user, 200, res);
 });
+
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    // Validate emil & password
+    if (!email || !password) {
+      return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+  
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+  
+    if (!user) {
+      return next(new ErrorResponse('Invalid credentials', 401));
+    }
+  
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+  
+    if (!isMatch) {
+      return next(new ErrorResponse('Invalid credentials', 401));
+    }
+    
+    const token = user.getSignedJwtToken();
+    res.status(200).json({success:true,token:token});
+    //sendTokenResponse(user, 200, res);
+});
+  

0adcfad035df57b79211ad09b4992fbdf6717b3e
Prakash <prkjha25@gmail.com>
2020-10-05 00:22:53 +0530

    auth protect middleware for access control

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -6,29 +6,34 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
-  bootcampPhotoUpload
+  bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
 
 const Bootcamp = require('../models/Bootcamp');
-const advancedResults =  require('../middleware/advancedResults');
+const advancedResults = require('../middleware/advancedResults');
 
 //include other resourse router
 const courseRouter = require('./courses');
 
 const router = express.Router();
 
-router.use('/:bootcampId/courses',courseRouter);
+const { protect } = require('../middleware/auth');
+
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route('/').get(advancedResults(Bootcamp,'courses'),getBootcamps).post(createBootcamp);
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(protect, createBootcamp);
 
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, bootcampPhotoUpload);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, updateBootcamp)
+  .delete(protect, deleteBootcamp);
 
 module.exports = router;

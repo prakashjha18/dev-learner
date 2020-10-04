4f212b08a1fca0d746c9022878311b949b43315a
Prakash <prkjha25@gmail.com>
2020-10-04 20:40:28 +0530

    register user and hash the password

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,5 +4,15 @@ const asyncHandler = require('../middleware/async')
 
 
 exports.register = asyncHandler(async(req,res,next) => {
+    const { name, email, password, role } = req.body;
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
     res.status(200).json({success:true});
+    //sendTokenResponse(user, 200, res);
 });

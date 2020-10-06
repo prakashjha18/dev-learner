15bb6c102b51cc5116a81f6577597bfc2fbd50a2
Prakash <prkjha25@gmail.com>
2020-10-07 02:41:43 +0530

    Rate Limiting and HPP CORS

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const errorHandler = require('./middleware/error');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -48,6 +51,20 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
+
 //set static folder
 app.use(express.static(path.join(__dirname,'public')));
 
